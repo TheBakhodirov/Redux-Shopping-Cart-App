cfaf096faa9201af3a499862a17172568a4263aa
TheBakhodirov <91797611+TheBakhodirov@users.noreply.github.com>
2022-12-07 23:46:09 +0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/uiSlice";
+let isFirstRender = true;
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+
+    const sendRequest = async () => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending request",
+          type: "warning",
+        })
+      );
+
+      const res = await fetch(
+        "https://redux-shop-cart-app-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Request sent to database successfully",
+          type: "success",
+        })
+      );
+    };
+    sendRequest().catch((err) => {
+      console.log(err.message);
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending request failed",
+          type: "error",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
+import CartItems from "./CartItems";
+import { useSelector } from "react-redux";
 const Layout = () => {
-  let total = 100;
+  const isCartShown = useSelector((state) => state.cart.showCart);
+  const total = useSelector((state) => state.cart.totalPrice);
 
   return (
     <React.Fragment>
       <div className="layout">
         <Header />
         <Products />
+        {isCartShown && <CartItems />}
         <div className="total-price">
           <h3>Total: ${total}</h3>
           <button className="orderBtn">Place Order</button>
